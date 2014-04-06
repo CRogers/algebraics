47ced0cceba65495dce902bdedf7009efdcafe23
Callum Rogers <callumr@palantir.com>
2014-04-06 14:28:18 -0700

    Added quotient rule

--- src/Differentiate.hs
+++ src/Differentiate.hs
@@ -8,6 +8,9 @@ differentiate' :: Expr -> Expr -> Expr
 differentiate' (Num _) _ = Num 0
 differentiate' (Sym _) _ = Num 1
 differentiate' (Mul a b) (Mul a' b') = Add (Mul a b') (Mul a' b)
+differentiate' (Div a b) (Div a' b') = Div top bottom
+	where top = Sub (Mul a' b) (Mul a b')
+	      bottom = Pow b (Num 2)
 differentiate' (Pow a b) (Pow a' _) = Mul a' (Mul b (Pow a (Sub b (Num 1))))
 differentiate' (Cos x) (Cos x') = Mul x' (Sub (Num 0) (Sin x))
 differentiate' (Sin x) (Sin x') = Mul x' (Cos x)
